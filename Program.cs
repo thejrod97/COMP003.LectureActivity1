bcc98eb0581ad7f51758e3d92cb776bab91a7e86
jjrod <jjrod@MSI>
2025-01-15 16:51:21 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+* Author: [Your name]
+* Course: [Your course name]
+* Faculty: Jonathan Cruz
+* Purpose: This application demonstrates the basic structure of a .NET console application. 
+*/
+
+// This namespace contains all the code for the application 
+namespace COMP003.LectureActivity1
+{
+    // This is the main class of the application 
+    internal class Program
+    {
+        // The entry point of the application 
+        static void Main(string[] args)
+        {
+            // Display a simple message 
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
